80b662223495e3e7c0592235f9b71d6185a001bf
Priyanshu <sethipriyanshu@outlook.com>
2024-06-06 11:54:47 -0500

    initialize new project and add Page Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { Home } from './pages/home';
+import { Shop } from './pages/shop';
+import { Contact } from './pages/contact';
+import { About } from './pages/about';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path='/' element = {<Home/>}/>
+          <Route path='/shop' element = {<Shop/>}/>
+          <Route path='/contact' element = {<Contact/>}/>
+          <Route path='/about' element = {<About/>}/>
+        </Routes>
+
+      </Router>
+      
     </div>
   );
 }
---
+++ src/pages/contact.tsx
@@ -0,0 +1,3 @@
+export const Contact = () =>{
+    return <p>This is contact Page</p>
+}
