786658608a9e56e61a3a20575c27e29edc7298f3
Priyanshu <sethipriyanshu@outlook.com>
2024-06-07 06:53:39 -0500

    add login button

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-
 export const Navbar = () =>{
     return(
         <div className="navbar bg-base-100">
@@ -12,17 +11,8 @@ export const Navbar = () =>{
       <li><Link to="/shop">Shop</Link></li>
       <li><Link to="/about">About</Link></li>
       <li><Link to="/contact">Contact</Link></li>
-      <li>
-        <details>
-          <summary>
-            My Profile
-          </summary>
-          <ul className="p-2 bg-base-100 rounded-t-none">
-            <li><a>Link 1</a></li>
-            <li><a>Link 2</a></li>
-          </ul>
-        </details>
-      </li>
+     <li> <button className="btn btn-outline btn-warning header-button btn-sm">Login with Google</button></li>
+
     </ul>
   </div>
 </div>
